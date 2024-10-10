e6ef65b3c94f474189c7115f8572ba9c55b43730
iintorsureanu <61560085+iintorsureanu@users.noreply.github.com>
2024-10-10 19:13:47 +0000

    del solution files

--- bmi_v1/bmi_v1.py
+++
@@ -1,6 +0,0 @@
-
-groesse = float(input('Bitte die Grösse eingeben: '))
-gewicht = float(input('Bitte das Gewicht eingeben: '))
-
-bmi = gewicht / groesse ** 2
-print('BMI:', round(bmi,2))
--- hello_name/hello_name.py
+++
@@ -1,7 +0,0 @@
-# Program hello_name.py
-
-name = input('Wie heisst du ? ')
-if name == '':
-    print('Hello, world!')
-else:
-    print('Hello,', name.title(), '!')
